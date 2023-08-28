258fb52d7b8aca0818bced641c7828f8ffec2bd0
freshy-hussain <122114478+freshy-hussain@users.noreply.github.com>
2023-08-28 12:52:56 -0400

    flip blue and white

--- divi-child/script.js
+++ divi-child/script.js
@@ -4,11 +4,15 @@
 jQuery(document).ready(function($){
     if($('.et_pb_accordion').length) {
         $('.et_pb_accordion').each(function(){
+            let section = $(this).parent().parent().parent();
             let row = $(this).parent().parent();
             let column = $(this).parent();
-            if (row.css('background-color') == '#2F9FA8' || row.css('background-color') == 'rgb(47, 159, 168)' || column.css('background-color') == '#2F9FA8' || column.css('background-color') == 'rgb(47, 159, 168)') {
-                $(':root').css('--accordion-toggle-color', '#ffffff');
+            if (section.css('background-color') == '#ffffff' || section.css('background-color') == '#fff' || section.css('background-color') == 'rgb(255, 255, 255)' || 
+                row.css('background-color') == '#ffffff' || row.css('background-color') == '#fff' || row.css('background-color') == 'rgb(255, 255, 255)' || 
+                column.css('background-color') == '#ffffff' || column.css('background-color') == '#fff'|| column.css('background-color') == 'rgb(255, 255, 255)') {
+                $(':root').css('--accordion-toggle-color', '#172b54');
             }
+            console.log(section.css('background-color'));
         })
     }
 })
