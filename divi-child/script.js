228b8d00b14b32964887687a0077ad4dc0174d42
freshy-hussain <122114478+freshy-hussain@users.noreply.github.com>
2023-08-28 13:17:28 -0400

    Update script.js

--- divi-child/script.js
+++ divi-child/script.js
@@ -4,15 +4,19 @@
 jQuery(document).ready(function($){
     if($('.et_pb_accordion').length) {
         $('.et_pb_accordion').each(function(){
-            let section = $(this).parent().parent().parent();
-            let row = $(this).parent().parent();
-            let column = $(this).parent();
-            if (section.css('background-color') == '#ffffff' || section.css('background-color') == '#fff' || section.css('background-color') == 'rgb(255, 255, 255)' || 
-                row.css('background-color') == '#ffffff' || row.css('background-color') == '#fff' || row.css('background-color') == 'rgb(255, 255, 255)' || 
-                column.css('background-color') == '#ffffff' || column.css('background-color') == '#fff'|| column.css('background-color') == 'rgb(255, 255, 255)') {
+            let section = $(this).parent().parent().parent().css('background-color');
+            let row = $(this).parent().parent().css('background-color');
+            let column = $(this).parent().css('background-color');
+            
+            if (section == 'rgb(255, 255, 255)' && row == 'rgba(0, 0, 0, 0)' && column == 'rgba(0, 0, 0, 0)') {
                 $(':root').css('--accordion-toggle-color', '#172b54');
             }
-            console.log(section.css('background-color'));
+            else if (section == 'rgba(0, 0, 0, 0)' && row == 'rgb(255, 255, 255)' && column == 'rgba(0, 0, 0, 0)') {
+                $(':root').css('--accordion-toggle-color', '#172b54');
+            }
+            else if (section == 'rgba(0, 0, 0, 0)' && row == 'rgba(0, 0, 0, 0)' && column == 'rgb(255, 255, 255)') {
+                $(':root').css('--accordion-toggle-color', '#172b54');
+            }
         })
     }
 })
